fe64398c9f02abadd91970e244928b8bb7fba8ef
Hankyu Kim <hankkim@c5r1s5.42seoul.kr>
2020-11-21 19:37:14 +0900

    test

---
+++ test.c
@@ -0,0 +1,19 @@
+#include "test.h"
+#include <stdio.h>
+
+int main(void)
+{
+    char *str = "abc";
+
+
+    // test_c();
+    // test_s(NULL);
+    // test_s(str);
+    test_p(str);
+    // test_di();
+    // test_u();
+    // test_xX();
+    // test_pers();
+
+    return 0;
+}
---
+++ test.h
@@ -0,0 +1,35 @@
+
+#define DI      123
+#define C       'a'
+
+#define PRT_PER(F)   printf("\"%s\"\t\t|", #F); \
+                    ret = printf(#F); \
+                    printf("|\tret : %d\n", ret);
+
+#define PRT_W_PER(F, W)   printf("\"%s\"\t*:%2d\t\t|", #F, W); \
+                    ret = printf(#F, W); \
+                    printf("|\tret : %d\n", ret);
+
+#define PRT_WW_PER(F, W1, W2)   printf("\"%s\"\t*:%2d *:%2d\t\t|", #F, W1, W2); \
+                    ret = printf(#F, W1, W2); \
+                    printf("|\tret : %d\n", ret);
+
+#define PRT(F, A)   printf("\"%s\"\t\t|", #F); \
+                    ret = printf(#F, A); \
+                    printf("|\tret : %d\n", ret);
+
+#define PRT_W(F, W, A)   printf("\"%s\"\t*:%2d\t\t|", #F, W); \
+                    ret = printf(#F, W, A); \
+                    printf("|\tret : %d\n", ret);
+
+#define PRT_WW(F, W1, W2, A)   printf("\"%s\"\t*:%2d *:%2d\t\t|", #F, W1, W2); \
+                    ret = printf(#F, W1, W2, A); \
+                    printf("|\tret : %d\n", ret);
+
+void test_c();
+void test_s(char *str);
+void test_p(void* p);
+void test_di();
+void test_u();
+void test_xX();
+void test_pers();
